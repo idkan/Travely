22a90bfcf8d796cd395f204715b52a1079db80f0
Abraham Serena <abraham.serena.lk@gmail.com>
2022-01-16 17:27:36 -0600

    Added PlaceDetails layout

--- src/App.js
+++ src/App.js
@@ -1,21 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { CssBaseline, Grid } from '@material-ui/core';
+
+import { getPlacesData } from './api';
 
 import Header from './components/Header/Header';
 import List from './components/List/List';
 import Map from './components/Map/Map';
 
 const App = () => {
+    const [places, setPlaces] = useState([]);
+
+    const [coordinates, setCoordinates] = useState({});
+    const [bounds, setBounds] = useState(null);
+
+    useEffect(() => {
+        navigator.geolocation.getCurrentPosition(({ coords: { latitude, longitude } }) => {
+            setCoordinates({ lat: latitude, lng: longitude });
+        })
+    }, []);
+
+    useEffect(() => {
+        if (bounds) {
+            getPlacesData(bounds.sw, bounds.ne)
+                .then((data) => {
+                    setPlaces(data);
+                });
+        }
+    }, [coordinates, bounds]);
+
     return (
         <>
             <CssBaseline />
             <Header />
             <Grid container spacer={3} style={{ width: '100%' }}>
-                <Grid items xs={12} md={4}>
-                    <List />
+                <Grid item xs={12} md={4}>
+                    <List places={places} />
                 </Grid>
-                <Grid items xs={12} md={8}>
-                    <Map />
+                <Grid item xs={12} md={8}>
+                    <Map
+                        setCoordinates={setCoordinates}
+                        setBounds={setBounds}
+                        coordinates={coordinates}
+                    />
                 </Grid>
             </Grid>
         </>
--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -5,22 +5,10 @@ import PlaceDetails from '../PlaceDetails/PlaceDetails';
 
 import useStyles from './styles';
 
-const List = () => {
+const List = ({ places }) => {
     const classes = useStyles();
     const [type, setType] = useState('restaurants');
     const [rating, setRating] = useState('');
-
-    const places = [
-        { name: 'Cool Place' },
-        { name: 'Beest Beer' },
-        { name: 'Beest Steak' },
-        { name: 'Cool Place' },
-        { name: 'Beest Beer' },
-        { name: 'Beest Steak' },
-        { name: 'Cool Place' },
-        { name: 'Beest Beer' },
-        { name: 'Beest Steak' }
-    ]
 
     return (
         <div className={classes.container}>
