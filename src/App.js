c72c5de3346cf2d52e87241b61242272b67aa51d
Abraham Serena <abraham.serena.lk@gmail.com>
2022-01-17 22:14:32 -0600

    Added ChipCard to Map Component

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Map from './components/Map/Map';
 
 const App = () => {
     const [places, setPlaces] = useState([]);
+    const [childClicked, setChildClicked] = useState(null);
 
     const [coordinates, setCoordinates] = useState({});
     const [bounds, setBounds] = useState(null);
@@ -31,13 +32,18 @@ const App = () => {
             <Header />
             <Grid container spacer={3} style={{ width: '100%' }}>
                 <Grid item xs={12} md={4}>
-                    <List places={places} />
+                    <List
+                        places={places}
+                        childClicked={childClicked}
+                    />
                 </Grid>
                 <Grid item xs={12} md={8}>
                     <Map
                         setCoordinates={setCoordinates}
                         setBounds={setBounds}
                         coordinates={coordinates}
+                        places={places}
+                        setChildClicked={setChildClicked}
                     />
                 </Grid>
             </Grid>
--- src/components/PlaceDetails/PlaceDetails.jsx
+++ src/components/PlaceDetails/PlaceDetails.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Box, Typography, Button, Card, CardMedia, CardContent, CardActions, Chip } from '@material-ui/core';
 import LocationOnIcon from '@material-ui/icons/LocationOn';
 import PhoneIcon from '@material-ui/icons/Phone';
-import Rating from '@material-ui/lab';
+import Rating from '@material-ui/lab/Rating';
 
 import useStyles from './styles';
 
@@ -18,6 +18,10 @@ const PlaceDetails = ({ place }) => {
             />
             <CardContent>
                 <Typography gutterBottom variant='h5'>{place.name}</Typography>
+                <Box display='flex' justifyContent='space-between'>
+                    <Rating name="read-only" value={Number(place.rating)} readOnly />
+                    <Typography gutterBottom variant='subtitle1'>Out of {place.num_reviews} reviews</Typography>
+                </Box>
                 <Box display='flex' justifyContent='space-between'>
                     <Typography variant='subtitle1'>Price</Typography>
                     <Typography gutterBottom variant='subtitle1'>{place.price_level}</Typography>
