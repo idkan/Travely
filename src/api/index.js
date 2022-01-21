b584d2d553c4ed5bac0a0c65c425c22568d181fb
Abraham Serena <abraham.serena.lk@gmail.com>
2022-01-20 19:23:43 -0600

    Added Weather Information with RapidApi API

--- src/api/index.js
+++ src/api/index.js
@@ -20,3 +20,19 @@ export const getPlacesData = async (type, sw, ne) => {
         console.log(error);
     }
 }
+
+export const getWeatherData = async (lat, lng) => {
+    try {
+        const { data } = await axios.get('https://community-open-weather-map.p.rapidapi.com/find', {
+            params: { lon: lng, lat: lat },
+            headers: {
+                'x-rapidapi-host': 'community-open-weather-map.p.rapidapi.com',
+                'x-rapidapi-key': ''
+            }
+        });
+
+        return data;
+    } catch (error) {
+        console.log(error);
+    }
+}
